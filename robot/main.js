cdac5f1edf84f3a579a001b0883d10860dd7e35f
Jkvelagapudi <jkvelagapudi@gmail.com>
2023-09-20 18:47:13 -0500

    cube added

--- robot/main.js
+++ robot/main.js
@@ -1,13 +1,12 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( '#ffffff');
+scene.background = new THREE.Color('#ffffff');
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-renderer.setSize(w, h, false);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1);
 const material = new THREE.MeshBasicMaterial( { color: "red" } );
