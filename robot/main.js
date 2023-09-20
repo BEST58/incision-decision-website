931abf55247e954fb971a0aa5f1daf3a4979b69c
Jkvelagapudi <jkvelagapudi@gmail.com>
2023-09-20 18:39:13 -0500

    Added cube without background

--- robot/main.js
+++ robot/main.js
@@ -1,19 +1,22 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color( '#ffffff');
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+renderer.setSize(w, h, false);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: "#FFFFFF" } );
+const geometry = new THREE.BoxGeometry( 1, 1, 1);
+const material = new THREE.MeshBasicMaterial( { color: "red" } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
-camera.rotation.x = -2
+cube.rotation.y = -0.5;
+
 
 function animate() {
 	requestAnimationFrame( animate );
